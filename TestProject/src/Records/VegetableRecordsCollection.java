9cbc5a299d551938bc020d5cc4ecfffe0414d58f
Harald Hamrin Reinhed <haraldhr@kth.se>
2024-01-18 15:04:58 +0100

    stuff

--- TestProject/src/Records/VegetableRecordsCollection.java
+++ TestProject/src/Records/VegetableRecordsCollection.java
@@ -2,53 +2,40 @@ package Records;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 import java.util.ArrayList;
 
 public class VegetableRecordsCollection {
     public static void main(String[] args) {
-        System.out.println("Start");
-        /* 
-        ArrayList<VegetableRecord> vegetableRecords = readVegetableRecordsFromFile("vegetable_data.txt");
+        
+        ArrayList<VegetableRecord> vegetableRecords = readData("vegetable_data.txt");
 
+        /*
         // Sort the list with vegetable records.
         ArrayList<VegetableRecord> filteredRecords = filterRecords(vegetableRecords);
 
-                // Skriv ut resultaten
+                // Print the results
         for (VegetableRecord record : vegetableRecords) {
                     System.out.println(record);
                 }
-                */
+        */
     }
 
     private static ArrayList<VegetableRecord> filterRecords(ArrayList<VegetableRecord>records){
         return new ArrayList<>();
     };
 
-    private static ArrayList<VegetableRecord> readVegetableRecordsFromFile(String fileName) { // Reads the data from the file.
+    private static ArrayList<VegetableRecord> readData(String fileName) { // Reads the data from the file.
         ArrayList<VegetableRecord> records = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
-            String currentLine;
-            while ((currentLine = reader.readLine()) != null) { // We continue reading until there's nothing left to read.
-                // The data in the file is formated as: type, country, size, unit.
-                
-                String[] parts = currentLine.split(", ");
-                if (parts.length == 4) { // I.e. if all of the information for the vegetable is present, if for example unit is missing length isn't 4
-                    String vegetableType = parts[0];
-                    String country = parts[1];
-                    int recordSize = Integer.parseInt(parts[2]);
-                    String unit = parts[3];
-
-                    // Att this new Vegetable to the list of records.
-                    records.add(new VegetableRecord(vegetableType, country, recordSize, unit));
-                }
-            }
-        } catch (Exception e) { // If an error occurs we simply don't add the vegetable.
-            //e.printStackTrace();
+        while(scanner.hasNext()){
+            String line = scanner.nextLine();
+            System.out.println(line);
         }
-
         return records;
     }
 }
--- TestProject/src/kth_javap_pathfinder.java
+++ TestProject/src/kth_javap_pathfinder.java
@@ -1,10 +1,6 @@
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Deque;
 import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class kth_javap_pathfinder {
@@ -46,18 +42,20 @@ public class kth_javap_pathfinder {
         boolean[][] alreadyCheckedTiles = new boolean[M][N]; //So that we don't check the same tile multiple times.
         HashSet<Character> alreadyClearedPaths = new HashSet<>(); //A Set of all the chars that have functioning paths.
         kth_javap_pathfinder pathfinder = new kth_javap_pathfinder();
+        Deque<int[]> tilesToCheck = new ArrayDeque<>();
 
         for (int i = 0; i < N; i++){ //For every letter in the first row (i.e. for every start tile)
+            
             char letter = map[0][i];
-
+            
             if(!(alreadyClearedPaths.contains(letter))){ //Only check the path from this starting tile if we haven't already found a path with this letter.
+                
+                /*
+                Currently using it as a stack to perform DFS (Depth-FirstSearch)
+                But if I change from .push() to .add() further down I instead use it as a queue and perform BFS (Breadth-First Search)
+                */
 
-            /*Currently using it as a stack to perform DFS (Depth-FirstSearch)
-            //But if I change from .push() to .add() further down I instead use it as a queue and perform BFS (Breadth-First Search)
-             */
-            Deque<int[]> tilesToCheck = new ArrayDeque<>();
-
-            tilesToCheck.add(new int[]{0, i});
+                tilesToCheck.add(new int[]{0, i});
                 while(!tilesToCheck.isEmpty()){
                     int[] nextTile = tilesToCheck.poll();
                     if (pathfinder.checkTile(alreadyCheckedTiles, nextTile[0], nextTile[1], letter, map, tilesToCheck)){
@@ -67,6 +65,7 @@ public class kth_javap_pathfinder {
                     }
                     alreadyCheckedTiles[nextTile[0]][nextTile[1]] = true;
                 }
+                tilesToCheck.clear();
             }
         }
 
@@ -82,41 +81,38 @@ public class kth_javap_pathfinder {
     }
 
 
-    private void getNextTiles(int currentRow, int currentColumn, boolean[][] alreadyCheckedTiles, char[][] map, Deque<int[]> tilesToCheck){
+    private void getNextTiles(int currentRow, int currentColumn, boolean[][] alreadyCheckedTiles, char[][] map, Deque<int[]> tilesToCheck, char letter){
         
+        // We check in the order UP, LEFT, RIGHT, DOWN
+        // We basically check the least likely to work directions first in order to quickly find dead-ends with DFS (Depth-First Search)
+        // We do this in order to clear the deque of impossible paths quicker to use less memory.
         int[][] directionsToCheck = new int[][]{}; //Empty to start with.
         if(currentRow == 0 && currentColumn == 0){ //Don't have to check either up or left.
-            directionsToCheck = new int[][] {this.DOWN, this.RIGHT};
+            directionsToCheck = new int[][] {this.RIGHT, this.DOWN};
         }
         else if(currentRow == 0 && currentColumn == map[0].length - 1){ // Not up or right.
-            directionsToCheck = new int[][]{this.DOWN, this.LEFT};
+            directionsToCheck = new int[][]{this.LEFT, this.DOWN};
         }
         else if (currentRow == 0){
-            directionsToCheck = new int[][]{this.DOWN, this.RIGHT, this.LEFT}; //We don't have to check the row above because there is none.
+            directionsToCheck = new int[][]{this.LEFT, this.RIGHT, this.DOWN}; //We don't have to check the row above because there is none.
         }
         else if(currentColumn == 0){ // We don't have to check to the left.
-            directionsToCheck = new int[][]{this.DOWN,  this.RIGHT, this.UP};
+            directionsToCheck = new int[][]{this.UP,  this.RIGHT, this.DOWN};
         }
         else if (currentColumn == map[0].length - 1){ //If we're all the way to the right there is no tile to the right.
-            directionsToCheck = new int[][] {this.DOWN, this.LEFT ,this.UP};
+            directionsToCheck = new int[][] {this.UP, this.LEFT ,this.DOWN};
         }
         else{ //Check all
-            directionsToCheck = new int[][] {this.DOWN, this.RIGHT, this.LEFT, this.UP};
+            directionsToCheck = new int[][] {this.UP, this.LEFT, this.RIGHT, this.DOWN};
         }
         for (int i = 0; i < directionsToCheck.length; i++){            
-            //if the correct letter is there and we haven't already checked that tile.
             
-            boolean alreadyChecked = false;
             int neighborRowIndex = currentRow + directionsToCheck[i][0];
             int neighborColIndex = currentColumn + directionsToCheck[i][1];
             
 
-            if(alreadyCheckedTiles[neighborRowIndex][neighborColIndex]){
-                alreadyChecked = true;
-            }
-
-            //System.out.println(map[currentRow + indexMods[i][0]][currentColumn + indexMods[i][1]]);
-            if (map[currentRow + directionsToCheck[i][0]][currentColumn + directionsToCheck[i][1]] == map[currentRow][currentColumn] && !alreadyChecked){
+            //if the correct letter is there and we haven't already checked that tile.
+            if (map[currentRow + directionsToCheck[i][0]][currentColumn + directionsToCheck[i][1]] == letter && !alreadyCheckedTiles[neighborRowIndex][neighborColIndex] ){
                 tilesToCheck.push(new int[]{neighborRowIndex, neighborColIndex});
             }
         }
@@ -129,7 +125,7 @@ public class kth_javap_pathfinder {
             return true;
         }
 
-        getNextTiles(currentRow, currentColumn, alreadyCheckedTiles, map, tilesToCheck); //Gets the neighboring tiles that have the same letter.
+        getNextTiles(currentRow, currentColumn, alreadyCheckedTiles, map, tilesToCheck, letter); //Gets the neighboring tiles that have the same letter.
 
         return false; //If we get all the way to here without returning true, there isn't a path.
     }
